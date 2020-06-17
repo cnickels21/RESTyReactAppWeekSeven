4a7e09e70b127f28813fa8efd65ee5de9fcbe9d7
cnickels21 <cnickels_21@yahoo.com>
2020-06-16 22:15:19 -0500

    Updates

---
+++ precache-manifest.603c70128784929213a0741559ff1afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716379ee1e8d52b72fdbc6c7439e06ed",
+    "url": "/RESTyReactAppWeekSeven%20/index.html"
+  },
+  {
+    "revision": "66708ec60dec4cc80bfb",
+    "url": "/RESTyReactAppWeekSeven%20/static/css/main.f24ae757.chunk.css"
+  },
+  {
+    "revision": "0828aecb8d1951d6e3e4",
+    "url": "/RESTyReactAppWeekSeven%20/static/js/2.e37d5b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTyReactAppWeekSeven%20/static/js/2.e37d5b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66708ec60dec4cc80bfb",
+    "url": "/RESTyReactAppWeekSeven%20/static/js/main.0b3e4eda.chunk.js"
+  },
+  {
+    "revision": "aa1c1bb4f1d9688faa5a",
+    "url": "/RESTyReactAppWeekSeven%20/static/js/runtime-main.3ff2afa3.js"
+  }
+]);
