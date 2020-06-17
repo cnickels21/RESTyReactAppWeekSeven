809692777eff93810eebd6e573799b6b411a8857
cnickels21 <cnickels_21@yahoo.com>
2020-06-16 21:30:45 -0500

    starter code copied in and base application setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './app.scss';
+
+// Let's talk about using index.js and some other name in the component folder
+// There's pros and cons for each way of doing this ...
+import Header from './components/header';
+import Footer from './components/footer';
+import Index from './components/index';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Index />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
