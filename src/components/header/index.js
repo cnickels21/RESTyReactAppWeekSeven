2e7b72175926b174a3d21ec944fa500bf5b25fba
cnickels21 <cnickels_21@yahoo.com>
2020-06-18 22:19:04 -0500

    nav links implemented

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>RESTy</h1>
+        <nav>
+            <ul classname="nav">
+                <li exact path="/"><NavLink exact to="/">Home</NavLink></li>
+                <li exact path="/history"><NavLink exact to="/history">History</NavLink></li>
+            </ul>
+        </nav>
       </header>
     );
   }
--- src/components/history/index.js
+++ src/components/history/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 class History extends React.Component {
-    constructor(props){
-        super(props);
-    }
+    // Don't need this yet
+    // constructor(props){
+    //     super(props);
+    // }
 
     render(){
         return (
